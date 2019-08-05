e6a1212670032f88feb97413378d0b0b3d09524b
aali2s <40173164+aali2s@users.noreply.github.com>
2019-08-06 00:17:09 +0200

    06August2019

--- demoApp/src/main/java/com/myschool/demoApp/App.java
+++ demoApp/src/main/java/com/myschool/demoApp/App.java
@@ -2,21 +2,29 @@ package com.myschool.demoApp;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Configuration con = new Configuration();
-       SessionFactory sef = con.buildSessionFactory();
-       @SuppressWarnings("unused")
-	Session session = sef.openSession();
-       
-       
-    } 
+public class App {
+	public static void main(String[] args) {
+
+		dbs_tab_person person = new dbs_tab_person();
+		person.setFirstName("testFirstName");
+		person.setLastName("testLastName");
+		person.setAddress("tannenbusch mitte");
+		person.setPersonID(101);
+		person.setPhoneNr(123453433);
+		Configuration con = new Configuration().configure().addAnnotatedClass(dbs_tab_person.class);
+		SessionFactory sef = con.buildSessionFactory();
+
+		Session session = sef.openSession();
+		Transaction t = session.beginTransaction();
+
+		session.save(person);
+		t.commit();
+	}
 }
---
+++ demoApp/src/main/java/com/myschool/demoApp/dbs_tab_person.java
@@ -0,0 +1,78 @@
+package com.myschool.demoApp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class dbs_tab_person {
+	/**
+	 * @return the personID
+	 */
+	public int getPersonID() {
+		return person_ID;
+	}
+	/**
+	 * @param personID the personID to set
+	 */
+	
+	public void setPersonID(int personID) {
+		this.person_ID = personID;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return first_Name;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.first_Name = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return last_Name;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.last_Name = lastName;
+	}
+	/**
+	 * @return the phoneNr
+	 */
+	public int getPhoneNr() {
+		return phone_Nr;
+	}
+	/**
+	 * @param phoneNr the phoneNr to set
+	 */
+	public void setPhoneNr(int phoneNr) {
+		this.phone_Nr = phoneNr;
+	}
+	@Id
+	private int person_ID;
+	private String first_Name;
+	private String last_Name;
+	private int phone_Nr;
+	private String address;
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
